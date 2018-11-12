6cb3f854dd5e29808f3cfe9db53b693257c0ab69
pjvalentini <pj.valentini@gmail.com>
2018-11-12 14:31:25 -0500

    refactored intial onclick

---
+++ assets/javascript/todoApp.js
@@ -0,0 +1,23 @@
+// adding check off of todos by clicking
+// $("li").click(function() {
+//     // checking if its gray to its rgb value...it will not work out other wise.
+//     if($(this).css("color") === "rgb(128, 128, 128)") {
+//         // turn text black
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         });
+//     }
+//     // store styles in an object... that targets the lis
+//     else {
+//          $(this).css({
+//              color: "grey",
+//              textDecoration: "line-through"
+//          });
+//     }
+// });
+
+// or with three lines of code!
+$("li").click(function () {
+    $(this).toggleClass("completed");
+});
