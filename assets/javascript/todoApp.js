d5add1b2407fcd593fbb26d92f1a7023de846a72
pjvalentini <pj.valentini@gmail.com>
2018-11-13 16:54:48 -0500

    finished basic functionality

--- assets/javascript/todoApp.js
+++ assets/javascript/todoApp.js
@@ -20,12 +20,34 @@
 // EVENT BUBBLING...
 
 // or with three lines of code!
-$("li").click(function() {
+// linethrough the finished list items
+// "li" argument states will have events affect them witht he page loads 
+// and takes in to account for future events.
+$("ul").on("click", "li",  function() {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+// deletes the list items after completion or if you make a mistake.
+// "span" argument states that event should only fire for an li inside of a ul.
+$("ul").on("click", "span", function(event) {
     console.log("span")
-    // stops event bubbling...
+    // .parent() can select the li the span is in.
+    // that this refers to the span
+    $(this).parent().fadeOut(1000, function() {
+        // this this refers to the li
+        $(this).remove();
+    });
+    // stops event bubbling to other elements
     event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event) {
+    // checks that the enter key is pressed. (enter - 13)
+    if(event.which === 13) {
+    // grabbing the text from the input.
+       var toDoText = $(this).val();
+       $(this).val("");
+       // create a new li and add to ul.
+       $("ul").append("<li><span>X</span> " + toDoText + "</li>");
+    }
+})
